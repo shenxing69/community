2fa6cc281364fcde144a6687dad1b85753199d64
wan <972303533@qq.com>
2019-09-14 11:46:30 +0800

    init repo

---
+++ src/main/java/com/wan/communtify/CommuntifyApplication.java
@@ -0,0 +1,13 @@
+package com.wan.communtify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuntifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuntifyApplication.class, args);
+	}
+
+}
