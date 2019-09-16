b2930014ef15a0d5d33e76d3e6dae5ea05861e17
wan <972303533@qq.com>
2019-09-16 23:52:34 +0800

    我的问题

--- src/main/java/com/wan/communtify/mapper/QuestionMapper.java
+++ src/main/java/com/wan/communtify/mapper/QuestionMapper.java
@@ -17,4 +17,8 @@ public interface QuestionMapper {
     List<Question> list(@Param("offset") Integer offset, @Param("size") Integer size);
     @Select("select count(1) from question")
     Integer count();
+    @Select("Select * from question where creator=#{userId} limit #{offset},#{size}")
+    List<Question> listByUerId(@Param("userId")int userId, @Param("offset")Integer offset, @Param("size")Integer size);
+    @Select("select count(1)  from question where creator=#{userId}")
+    Integer countByuserId(int userId);
 }
--- src/main/java/com/wan/communtify/service/QuestionService.java
+++ src/main/java/com/wan/communtify/service/QuestionService.java
@@ -20,12 +20,21 @@ public class QuestionService {
     @Autowired
     private QuestionMapper questionMapper;
     public PaginationDTO list(Integer page, Integer size) {
-        Integer offset=size*(page-1);
+
         PaginationDTO paginationDTO=new PaginationDTO();
+        Integer totalPage;
         Integer totalCount=questionMapper.count();
-        paginationDTO.setPagination(totalCount,page,size);
+
+        if(totalCount%size==0){
+            totalPage=totalCount/size;
+        }else{
+            totalPage=totalCount/size +1;
+        }
+
         if(page<1){page=1;}
-        if(page>paginationDTO.getTotalPage()){page=paginationDTO.getTotalPage();}
+        if(page>totalPage){page=totalPage;}
+        paginationDTO.setPagination(totalPage,page);
+        Integer offset=size*(page-1);
         //尝试使用联合查询代替循环赋值，P23
         // 算了如果使用级联查询还需要设置resultMap，用注解不好写，不想创建mapper，而且user级联属性还很多，手写要累死
         List<Question> questions = questionMapper.list(offset,size);
@@ -39,7 +48,36 @@ public class QuestionService {
             questionDTOList.add(questionDTO);
         }
         paginationDTO.setQuestions(questionDTOList);
-        //System.out.println(paginationDTO.getPages().toString());
+        return paginationDTO;
+    }
+
+    public PaginationDTO list(Integer userId, Integer page, Integer size) {
+        PaginationDTO paginationDTO=new PaginationDTO();
+        Integer totalPage;
+        Integer totalCount=questionMapper.countByuserId(userId);
+
+        if(totalCount%size==0){
+            totalPage=totalCount/size;
+        }else{
+            totalPage=totalCount/size +1;
+        }
+
+        if(page<1){page=1;}
+        if(page>totalPage){page=totalPage;}
+        paginationDTO.setPagination(totalPage,page);
+        Integer offset=size*(page-1);
+
+        List<Question> questions = questionMapper.listByUerId(userId,offset,size);
+        List<QuestionDTO> questionDTOList=new ArrayList<>();
+
+        for(Question question:questions){
+            User user= usermapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question,questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        paginationDTO.setQuestions(questionDTOList);
         return paginationDTO;
     }
 }
